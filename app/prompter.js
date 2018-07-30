7abbb64bc9281a07cce03aceae9994a6f4624666
Plamen Petkov <plamen.petkov@paysafe.com>
2018-07-30 03:31:58 +0300

    fixed prompter message

--- app/prompter.js
+++ app/prompter.js
@@ -8,20 +8,27 @@ function runPromptWithQuestion(missingFromIosFileName, missingFromAndroidFileNam
         const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout,
-            prompt: `I'm about to start the migration of the android and ios strings.
-            Before you make your choice:
-                    Know you can go in the ios or android file and leave only the keys that you want translated:
-                    * iOS keys that will be migrated: "${missingFromIosFileName}"
-                    * Android keys that will be migrated: "${missingFromAndroidFileName}"
-                    
-                    Feel free to delete the lines with the keys you don't want to migrate and make your choice!
+            prompt: `
+
+I'm about to start the migration of the android and ios strings.
+    Before you make your choice:
+            Know you can go in the ios or android file and leave only the keys that you want translated:
+            * iOS keys that will be migrated: "${missingFromIosFileName}"
+            * Android keys that will be migrated: "${missingFromAndroidFileName}"
             
-            Your options:
-                    1) migrate only ios
-                    2) migrate only android
-                    3) migrate both
-                    4) cancel
-                `
+            ############################################## W A R N I N G #######################################################
+            #   Feel free to delete the lines with the keys you don't want to migrate and make your choice!                    #
+            #   If you're planing on changing the ios migration file know the file is saved in "UTF-16LE" format!              #
+            #   That means, if you want to make changes to it, be sure to save it as a "UTF-16LE" after you're done editing!   #
+            ####################################################################################################################
+    
+    Your options:
+            1) migrate only ios
+            2) migrate only android
+            3) migrate both
+            4) cancel
+
+>> Awaiting your responce: `
         });
 
         if (process.stdin.isTTY) {
